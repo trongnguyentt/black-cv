b03a3a2c1387a01014b189b487cefff663fc1a66
XUANHAVU <xuanha.tlu9x@gmail.com>
2020-03-16 17:26:01 +0700

    test

--- src/main/webapp/app/entities/company/company.module.ts
+++ src/main/webapp/app/entities/company/company.module.ts
@@ -8,10 +8,18 @@ import { CompanyUpdateComponent } from './company-update.component';
 import { CompanyDeleteDialogComponent } from './company-delete-dialog.component';
 import { companyRoute } from './company.route';
 import { UserCompanyComponent } from 'app/entities/company/user-company.component';
+import { CompanyDetailUserComponent } from 'app/entities/company/company-detail-user.component';
 
 @NgModule({
   imports: [BlackcvSharedModule, RouterModule.forChild(companyRoute)],
-  declarations: [UserCompanyComponent, CompanyComponent, CompanyDetailComponent, CompanyUpdateComponent, CompanyDeleteDialogComponent],
+  declarations: [
+    CompanyDetailUserComponent,
+    UserCompanyComponent,
+    CompanyComponent,
+    CompanyDetailComponent,
+    CompanyUpdateComponent,
+    CompanyDeleteDialogComponent
+  ],
   entryComponents: [CompanyDeleteDialogComponent]
 })
 export class BlackcvCompanyModule {}
